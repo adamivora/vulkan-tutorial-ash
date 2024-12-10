c84204f47459c99f7c5a31090da1c777e36d19de
Adam Ivora <xivora@fi.muni.cz>
2024-12-10 21:10:56 +0100

    Instance

--- src/bin/tut.rs
+++ src/bin/tut.rs
@@ -1,13 +1,16 @@
-use ash::util::*;
 use ash::vk;
 
+use ash::Entry;
+use ash::Instance;
 use winit::application::ApplicationHandler;
-use winit::dpi::Size;
 use winit::error::EventLoopError;
 use winit::event::WindowEvent;
 use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
-use winit::platform::run_on_demand::EventLoopExtRunOnDemand;
+use winit::raw_window_handle::HasDisplayHandle;
 use winit::window::{Window, WindowId};
+
+use std::ffi::CString;
+use std::process;
 
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
@@ -15,6 +18,7 @@ const HEIGHT: u32 = 600;
 #[derive(Default)]
 struct App {
     window: Option<Window>,
+    instance: Option<Instance>,
 }
 
 impl App {
@@ -26,23 +30,88 @@ impl App {
         self.window = Some(event_loop.create_window(window_attributes).unwrap());
     }
 
-    fn init_vulkan(&mut self) {}
+    fn create_instance(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        let application_name = CString::new("Hello Triangle").unwrap();
+        let engine_name = CString::new("No Engine").unwrap();
 
-    fn cleanup(&self) {}
+        let app_info = vk::ApplicationInfo::default()
+            .application_name(&application_name)
+            .application_version(vk::make_api_version(0, 1, 0, 0))
+            .engine_name(&engine_name)
+            .engine_version(vk::make_api_version(0, 1, 0, 0))
+            .api_version(vk::API_VERSION_1_0);
+
+        let mut extensions = ash_window::enumerate_required_extensions(
+            self.window.as_ref().unwrap().display_handle()?.as_raw(),
+        )
+        .unwrap()
+        .to_vec();
+
+        #[cfg(any(target_os = "macos", target_os = "ios"))]
+        {
+            extensions.push(ash::khr::portability_enumeration::NAME.as_ptr());
+            extensions.push(ash::khr::get_physical_device_properties2::NAME.as_ptr());
+        }
+
+        let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
+            vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
+        } else {
+            vk::InstanceCreateFlags::default()
+        };
+
+        let layer_names = Vec::new();
+        let create_info = vk::InstanceCreateInfo::default()
+            .application_info(&app_info)
+            .enabled_extension_names(&extensions)
+            .enabled_layer_names(&layer_names)
+            .flags(create_flags);
+
+        let entry = Entry::linked();
+
+        self.instance = Some(unsafe { entry.create_instance(&create_info, None)? });
+        let supported_extensions = unsafe { entry.enumerate_instance_extension_properties(None)? };
+
+        println!(
+            "Supported extensions:\n{}",
+            supported_extensions
+                .iter()
+                .fold(String::new(), |acc, &num| acc
+                    + "\t"
+                    + num.extension_name_as_c_str().unwrap().to_str().unwrap()
+                    + "\n")
+        );
+
+        Result::Ok(())
+    }
+
+    fn init_vulkan(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+        self.create_instance()
+    }
+
+    fn cleanup(&mut self) {
+        unsafe {
+            if self.instance.is_some() {
+                self.instance.as_mut().unwrap().destroy_instance(None);
+            }
+        }
+    }
 }
 
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
         App::init_window(self, event_loop);
-        App::init_vulkan(self);
+        App::init_vulkan(self).unwrap_or_else(|err| {
+            eprintln!("{err}");
+            process::exit(1);
+        });
     }
 
     fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => {
                 println!("The close button was pressed; stopping");
-                event_loop.exit();
                 App::cleanup(self);
+                event_loop.exit();
             }
             WindowEvent::RedrawRequested => {}
             _ => (),
