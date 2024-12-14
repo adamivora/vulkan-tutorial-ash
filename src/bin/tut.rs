ea67c6192c59b1ffe98d2bd9717fc1538615d8ed
Adam Ivora <xivora@fi.muni.cz>
2024-12-14 18:22:52 +0100

    Validation layers

--- src/bin/tut.rs
+++ src/bin/tut.rs
@@ -1,5 +1,8 @@
 use ash::vk;
 
+use ash::ext::debug_utils;
+use ash::vk::DebugUtilsMessengerCreateInfoEXT;
+use ash::vk::DebugUtilsMessengerEXT;
 use ash::Entry;
 use ash::Instance;
 use winit::application::ApplicationHandler;
@@ -9,28 +12,71 @@ use winit::event_loop::{ActiveEventLoop, ControlFlow, EventLoop};
 use winit::raw_window_handle::HasDisplayHandle;
 use winit::window::{Window, WindowId};
 
-use std::ffi::CString;
+use std::borrow::Cow;
+use std::ffi::{CStr, CString};
+use std::os::raw::c_char;
 use std::process;
 
 const WIDTH: u32 = 800;
 const HEIGHT: u32 = 600;
 
-#[derive(Default)]
-struct App {
-    window: Option<Window>,
-    instance: Option<Instance>,
+const VALIDATION_LAYERS: [&CStr; 1] =
+    [unsafe { CStr::from_bytes_with_nul_unchecked(b"VK_LAYER_KHRONOS_validation\0") }];
+const ENABLE_VALIDATION_LAYERS: bool = cfg!(debug_assertions);
+
+unsafe extern "system" fn messenger_debug_callback(
+    _message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+    _message_type: vk::DebugUtilsMessageTypeFlagsEXT,
+    p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT<'_>,
+    _user_data: *mut std::os::raw::c_void,
+) -> vk::Bool32 {
+    let callback_data = *p_callback_data;
+    let message: Cow<'_, str> = if callback_data.p_message.is_null() {
+        Cow::from("")
+    } else {
+        CStr::from_ptr(callback_data.p_message).to_string_lossy()
+    };
+    eprintln!("validation layer: {message}\n",);
+    vk::FALSE
 }
 
-impl App {
-    fn init_window(&mut self, event_loop: &ActiveEventLoop) {
+struct Vulkan {
+    window: Window,
+    instance: Instance,
+    debug_utils_loader: ash::ext::debug_utils::Instance,
+    debug_callback: DebugUtilsMessengerEXT,
+}
+
+#[derive(Default)]
+struct App {
+    vulkan: Option<Vulkan>,
+}
+
+impl Vulkan {
+    fn new(event_loop: &ActiveEventLoop) -> Result<Vulkan, Box<dyn std::error::Error>> {
+        let window = Vulkan::init_window(event_loop);
+        let (instance, debug_utils_loader, debug_callback) = Vulkan::init_vulkan(&window)?;
+        Result::Ok(Self {
+            window,
+            instance,
+            debug_utils_loader,
+            debug_callback,
+        })
+    }
+
+    fn init_window(event_loop: &ActiveEventLoop) -> Window {
         let window_attributes = Window::default_attributes()
             .with_inner_size(winit::dpi::LogicalSize::new(WIDTH, HEIGHT))
             .with_title("Vulkan")
             .with_resizable(false);
-        self.window = Some(event_loop.create_window(window_attributes).unwrap());
+        event_loop.create_window(window_attributes).unwrap()
     }
 
-    fn create_instance(&mut self) -> Result<(), Box<dyn std::error::Error>> {
+    fn create_instance(window: &Window) -> Result<Instance, Box<dyn std::error::Error>> {
+        if ENABLE_VALIDATION_LAYERS && !Vulkan::check_validation_layer_support()? {
+            return Err("validation layers requested, but not available!".into());
+        }
+
         let application_name = CString::new("Hello Triangle").unwrap();
         let engine_name = CString::new("No Engine").unwrap();
 
@@ -41,17 +87,15 @@ impl App {
             .engine_version(vk::make_api_version(0, 1, 0, 0))
             .api_version(vk::API_VERSION_1_0);
 
-        let mut extensions = ash_window::enumerate_required_extensions(
-            self.window.as_ref().unwrap().display_handle()?.as_raw(),
-        )
-        .unwrap()
-        .to_vec();
+        let mut extensions =
+            ash_window::enumerate_required_extensions(window.display_handle()?.as_raw())?.to_vec();
 
         #[cfg(any(target_os = "macos", target_os = "ios"))]
         {
             extensions.push(ash::khr::portability_enumeration::NAME.as_ptr());
             extensions.push(ash::khr::get_physical_device_properties2::NAME.as_ptr());
         }
+        extensions.push(ash::ext::debug_utils::NAME.as_ptr());
 
         let create_flags = if cfg!(any(target_os = "macos", target_os = "ios")) {
             vk::InstanceCreateFlags::ENUMERATE_PORTABILITY_KHR
@@ -59,17 +103,33 @@ impl App {
             vk::InstanceCreateFlags::default()
         };
 
-        let layer_names = Vec::new();
-        let create_info = vk::InstanceCreateInfo::default()
+        let layer_names = if ENABLE_VALIDATION_LAYERS {
+            Vec::from(VALIDATION_LAYERS)
+        } else {
+            Vec::new()
+        };
+        let layer_names_raw: Vec<*const c_char> = layer_names
+            .iter()
+            .map(|raw_name| raw_name.as_ptr())
+            .collect();
+
+        let mut create_info = vk::InstanceCreateInfo::default()
             .application_info(&app_info)
             .enabled_extension_names(&extensions)
-            .enabled_layer_names(&layer_names)
+            .enabled_layer_names(&layer_names_raw)
             .flags(create_flags);
+
+        let mut debug_create_info = vk::DebugUtilsMessengerCreateInfoEXT::default();
+        if ENABLE_VALIDATION_LAYERS {
+            debug_create_info = Vulkan::populate_debug_messenger_create_info(debug_create_info);
+            create_info = create_info.push_next(&mut debug_create_info);
+        }
 
         let entry = Entry::linked();
 
-        self.instance = Some(unsafe { entry.create_instance(&create_info, None)? });
-        let supported_extensions = unsafe { entry.enumerate_instance_extension_properties(None)? };
+        let instance = unsafe { entry.create_instance(&create_info, None)? };
+        let supported_extensions: Vec<vk::ExtensionProperties> =
+            unsafe { entry.enumerate_instance_extension_properties(None)? };
 
         println!(
             "Supported extensions:\n{}",
@@ -81,36 +141,106 @@ impl App {
                     + "\n")
         );
 
-        Result::Ok(())
+        Result::Ok(instance)
     }
 
-    fn init_vulkan(&mut self) -> Result<(), Box<dyn std::error::Error>> {
-        self.create_instance()
+    fn check_validation_layer_support() -> Result<bool, Box<dyn std::error::Error>> {
+        let entry = Entry::linked();
+        let available_layers: Vec<vk::LayerProperties> =
+            unsafe { entry.enumerate_instance_layer_properties()? };
+
+        let layers_found = VALIDATION_LAYERS.iter().all(|&layer| {
+            available_layers
+                .iter()
+                .any(|&av_layer| av_layer.layer_name_as_c_str().unwrap() == layer)
+        });
+
+        Result::Ok(layers_found)
+    }
+
+    fn populate_debug_messenger_create_info<'a>(
+        create_info: DebugUtilsMessengerCreateInfoEXT<'a>,
+    ) -> DebugUtilsMessengerCreateInfoEXT<'a> {
+        create_info
+            .message_severity(
+                vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                    | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+            )
+            .message_type(
+                vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                    | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+                    | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+            )
+            .pfn_user_callback(Some(messenger_debug_callback))
+    }
+
+    fn setup_debug_messenger(
+        instance: &Instance,
+    ) -> Result<(ash::ext::debug_utils::Instance, DebugUtilsMessengerEXT), Box<dyn std::error::Error>>
+    {
+        if !ENABLE_VALIDATION_LAYERS {
+            return Result::Err("validation layers not enabled".into());
+        }
+
+        let create_info = Vulkan::populate_debug_messenger_create_info(
+            DebugUtilsMessengerCreateInfoEXT::default(),
+        );
+        let entry = Entry::linked();
+        let debug_utils_loader = debug_utils::Instance::new(&entry, instance);
+        let debug_callback =
+            unsafe { debug_utils_loader.create_debug_utils_messenger(&create_info, None)? };
+
+        Result::Ok((debug_utils_loader, debug_callback))
+    }
+
+    fn init_vulkan(
+        window: &Window,
+    ) -> Result<
+        (
+            Instance,
+            ash::ext::debug_utils::Instance,
+            DebugUtilsMessengerEXT,
+        ),
+        Box<dyn std::error::Error>,
+    > {
+        let instance = Vulkan::create_instance(window)?;
+        let (loader, callback) = Vulkan::setup_debug_messenger(&instance)?;
+        Result::Ok((instance, loader, callback))
     }
 
     fn cleanup(&mut self) {
         unsafe {
-            if self.instance.is_some() {
-                self.instance.as_mut().unwrap().destroy_instance(None);
+            if ENABLE_VALIDATION_LAYERS {
+                self.debug_utils_loader
+                    .destroy_debug_utils_messenger(self.debug_callback, None);
             }
+            self.instance.destroy_instance(None);
         }
     }
 }
 
+impl Drop for Vulkan {
+    fn drop(&mut self) {
+        self.cleanup();
+    }
+}
+
+impl App {}
+
 impl ApplicationHandler for App {
     fn resumed(&mut self, event_loop: &ActiveEventLoop) {
-        App::init_window(self, event_loop);
-        App::init_vulkan(self).unwrap_or_else(|err| {
+        self.vulkan = Some(Vulkan::new(event_loop).unwrap_or_else(|err| {
             eprintln!("{err}");
             process::exit(1);
-        });
+        }));
     }
 
-    fn window_event(&mut self, event_loop: &ActiveEventLoop, id: WindowId, event: WindowEvent) {
+    fn window_event(&mut self, event_loop: &ActiveEventLoop, _id: WindowId, event: WindowEvent) {
         match event {
             WindowEvent::CloseRequested => {
                 println!("The close button was pressed; stopping");
-                App::cleanup(self);
+                self.vulkan = None;
                 event_loop.exit();
             }
             WindowEvent::RedrawRequested => {}
